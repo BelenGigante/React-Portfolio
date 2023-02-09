2e115079ed742133f35e388da41f4440d543ef78
BelenGigante <vinn_a@hotmail.com>
2023-02-08 21:40:28 -0500

    footer icons and links working

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,19 +1,37 @@
 import React from 'react';
+//import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
+//import { MarkGithubIcon } from "@react-icons/all-files/fa/FaBeer";
+//<MarkGithubIcon size={16} />
 
 function NavLinks() {
 
   return (
-   
-    <footer>
-      <h3>Find me @</h3>
-      <ul >
-        <li >
-            <a href='https://github.com/BelenGigante'> GitHub    </a>
-          </li>
-      </ul>
-      </footer>
+    <div>
       
-      );
+      <footer className='footer'>
+        
+        <h3>Find me @</h3>
+        <ul className='footerList'>
+          <div >
+            <SocialIcon className='githubicon' url="https://github.com/BelenGigante" target="_blank"/>
+            <br></br>
+            <a href='https://github.com/BelenGigante' target="_blank"> GitHub </a>
+            </div>
+            <div>
+            <SocialIcon className='icon' url="https://www.linkedin.com/pulse/topics/home/" target="_blank"/>
+            <br></br>
+            <a href='https://www.linkedin.com/pulse/topics/home/' target="_blank"> LinkedIn </a>
+            </div>
+            <div>
+            <SocialIcon className='icon' url="https://twitter.com/explore" target="_blank"/>
+            <br></br>
+            <a href='https://twitter.com/explore' target="_blank"> Twitter </a>
+          </div>
+        </ul>
+      </footer>
+    </div>
+  );
 }
 
-      export default NavLinks;
+export default NavLinks;
